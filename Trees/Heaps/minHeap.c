67a73bf3fb817aae16b1f8f7280132e70f10bf76
Karley <karley.waguespack@gmail.com>
2022-07-12 22:59:15 -0500

    pushing some additions to the heap program

--- Trees/Heaps/minHeap.c
+++ Trees/Heaps/minHeap.c
@@ -46,64 +46,114 @@ void buildMinHeap(Heap* heap){
 
 //sinks down a single element to its correct position; expensive for nodes high in tree
 void sinkDown(Heap* heap, int nodeIndex){
-    int leftChild = 2*nodeIndex + 1;
-    int rightChild = 2*nodeIndex + 2;
+    int leftChild;
+    int rightChild;
+    int minimumInd;
 
-    //find the minimum of the two children
-    int minimumInd = findMinimum(heap->array, leftChild, rightChild);
     //continue swapping the node with the minimum child as long as node > minimum; 
     while((heap->array)[nodeIndex] > (heap->array)[minimumInd] && minimumInd < heap->size){ //NOTE: NEED TO FIX LAST CONDITION; MAY NOT WORK IF MINIMUMIND = -1
-         swap(&(heap->array)[nodeIndex], &(heap->array)[minimumInd]);
-         //remember to update the index of the node after swaps with min child
-         nodeIndex = minimumInd;
+        int leftChild = 2*nodeIndex + 1;
+        int rightChild = 2*nodeIndex + 2;
+        minimumInd = findMinimum(heap->array, leftChild, rightChild);
+        swap(&(heap->array)[nodeIndex], &(heap->array)[minimumInd]);
+        //remember to update the index of the node after swaps with min child
+        nodeIndex = minimumInd;
     }
 }
 
 
 
 //floats up a single element to its correct position; expensive for nodes towards bottom of tree
-void floatUp(/* Heap* heap, int nodeIndex */){
-    //parent index: (i-1)/2
-    //as long as the node is less than the parent AND as long as nodeIndex > 0
-    //if node is less than parent, perform a swap
+void floatUp(Heap* heap, int nodeIndex){
+    int parentIndex;
+    //as long as the node is less than the parent AND nodeIndex > 0
+    while((heap->array)[nodeIndex] < (heap->array)[parentIndex] && nodeIndex > 0){
+        parentIndex = (nodeIndex-1)/2;
+        //perform a swap and update the nodeIndex to parent
+        swap(&(heap->array)[nodeIndex], &(heap->array)[parentIndex]);
+        nodeIndex = parentIndex;
+    }
 }
 
 
 
-int extractMinimum(/* Heap* heap */){
-    //check for emptiness
-    //return the first element in the array
-    //overwrite the first element with the last element in the heap
-    //sink down the new root
-    //update size
+int extractMinimum(Heap* heap){
+    //make sure something is in the heap
+    if(heap -> size > 0){
+        int minimum = (heap -> array)[0];
+        int heapSize = heap->size;
+        //overwrite the first element with the last element in the heap
+        (heap -> array)[0] = (heap -> array)[heapSize-1];
+        //sink down the new root
+        sinkDown(heap, 0);
+        //update size
+        --(heap -> size);
+        return minimum;
+    }else{
+        printf("Cannot extract minimum. Nothing is within the heap.\n");
+    }
 }
 
 
 
-int getMinimum(/* Heap* heap */){
-    //check for emptiness
-    //simply return the first element in the array
+int getMinimum(Heap* heap){
+    //make sure something is in the heap
+    if(heap -> size > 0){
+        //simply return the first element in the array
+        return (heap->array)[0];
+    }else{
+        printf("Cannot get the minimum. Nothing is within the heap.\n");
+    }
 }
 
 
 
-void decreaseKey(/* Heap*  heap, int nodeIndex, int newVal*/){
+void decreaseKey(Heap*  heap, int nodeIndex, int newVal){
     //change the value of the node; it's assumed newVal will be smaller
+    (heap->array)[nodeIndex] = newVal;
     //apply the float up operation to the node
+    floatUp(heap, nodeIndex);
 }
 
 
-void deleteNode(/* Heap* heap, int nodeIndex*/){
-    //check for emptiness
-    //overwrite the node to be deleted with the last element in the heap
-    //sink down the element to it's correct spot
-    //update size
+void deleteNode(Heap* heap, int nodeIndex){
+    //make sure something is in the heap
+    if(heap -> size > 0){
+        int heapSize = heap->size;
+        //overwrite the node to be deleted with the last element in the heap
+        (heap->array)[nodeIndex] = (heap->array)[heapSize-1];
+        //sink down the element to it's correct spot
+        sinkDown(heap, nodeIndex);
+        //update size
+        --(heap -> size);
+    }else{
+        printf("Cannot delete the item. Nothing is within the heap.\n");
+    }
 }
 
 
-void insertNode(/* Heap* heap, int nodeIndex*/){
+void insertNode(Heap* heap, int value){
     //check for overflow
-    //put the node in the next free position in the array
-    //float it up to it's correct position
-    //update size
+    if(heap -> size == heap -> maxCapacity){
+        printf("Cannot add the node in the heap. There is not enough space.\n");
+    }else{
+        //put the node in the next free position in the array
+        (heap->array)[heap->size] = value;
+        //float it up to it's correct position
+        floatUp(heap, heap->size);
+        //update size
+        ++(heap -> size);
+    }
+}
+
+
+int main(){
+
+    int data[100] = {3, 8, 2, 9, 8, 2, 4, 1, 0, 0, 1, 8};
+
+    Heap* heap = initializeHeap(100, data);
+
+
+
+    return 0;
 }
