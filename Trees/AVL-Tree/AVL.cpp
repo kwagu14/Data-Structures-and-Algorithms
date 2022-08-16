3914bc58f57289714addc21c4ed4f2ca1257c775
kwaguespack <kwaguespack@stephensontechnology.org>
2022-08-16 12:18:24 -0500

    fixed the insertion method in avl tree program

--- Trees/AVL-Tree/AVL.cpp
+++ Trees/AVL-Tree/AVL.cpp
@@ -1,8 +1,8 @@
 //****************************************************************************************************
 // File Name:   AVL.cpp
-// Purpose:     AVL tree with insert and range minimum operations; CSC 3102 / Dr. Rahul Shah
-// Date:        10/17/2020
-// Author:      Karley Waguespack (kwagu14)
+// Purpose:     AVL tree with operations; originally programmed for CSC 3102, modified August 2022
+// Creation Date:        10/17/2020
+// Author:      Karley Waguespack
 //*****************************************************************************************************
 
 #include <iostream>
@@ -20,30 +20,28 @@ typedef struct _Node {
     _Node *left; 
     _Node *right;
     _Node *parent;
-    int size; //should be the number of nodes in total in all subtrees, plus this node itself
-    int height; //should be the longest path down to a leaf node, from this node.
-    int mindata; //should be the minimum data in this node's subtree
+    int size; //The number of nodes in total in all subtrees, plus this node itself
+    int height; //The longest path down to a leaf node, from this node.
+    int mindata; //The minimum data in this node's subtree
 
 } Node;
 
-
-
 //GLOBAL VARIABLES
-Node *root; //this will hold the root of the avl tree at any given time; it has to be initialized appropriately using createNode
-bool heightIncreased = false; //will flag if height changed
+Node *root; //this will hold the root of the AVL tree at any given time; it has to be initialized appropriately using createNode
+
 
 
 
 //creates a new node and initializes all of the values
-Node *createNode(int _key, int _data){
+Node *CreateNode(int key, int data){
 
     Node *p = NULL;
     p = (Node *) malloc(sizeof(Node));
 
-    p -> key = _key; p -> data = _data;
+    p -> key = key; p -> data = data;
     p -> size = 1; p -> height = 1; 
     p -> left = NULL; p -> right = NULL; p -> parent = NULL;
-    p -> mindata = _data;
+    p -> mindata = data;
 
     return p;
 
@@ -51,11 +49,8 @@ Node *createNode(int _key, int _data){
 
 
 
-//HELPER FUNCTIONS
-
-//this should be called on every node 
-//from bottom up
-void recalc_height(Node* x){
+//recalculates the height for a given node
+void Recalc_height(Node* x){
 
     if ((x -> left == NULL) && (x -> right == NULL)){
         //if both left and right pointers are null, the height is composed of only the node itself
@@ -75,9 +70,8 @@ void recalc_height(Node* x){
 
 
 
-//recalculates size for each node; should be called on node inserted as well
-//as all of the ancestors from bottom up. 
-void recalc_size(Node *x){
+//recalculates size for a node 
+void Recalc_size(Node *x){
     if ((x -> left == NULL) && (x -> right == NULL)){
         x -> size = 1; 
     } else if (x -> left == NULL){ 
@@ -91,9 +85,8 @@ void recalc_size(Node *x){
 
 
 
-//recalculates mindata for one node; after inserting, this should be recalculated for the 
-//node inserted as well as all of its ancestors from bottom up
-void recalc_mindata(Node *x){
+//recalculates mindata for one node
+void Recalc_mindata(Node *x){
 
     if (!(x -> left) && !(x -> right)){
         x -> mindata = x -> data;
@@ -115,43 +108,43 @@ void recalc_mindata(Node *x){
 
 
 
-//left rotates the input node and returns the new node so we can use it in hybrid cases
-//this will be used in a zig zig case [\]
-Node *l_rotate(Node *x){
-
+//left rotates the input node and returns the new node (y) so we can use it in hybrid cases
+Node *L_rotate(Node *x){
 
     Node *p = x -> parent; 
     Node *y = x -> right; 
     Node *z = y -> left; 
-
-    //perform rotation
-    y -> left = x; x -> parent = y; 
+    //y becomes parent of x and x becomes left child of y
+    y -> left = x; 
+    x -> parent = y; 
+    //z becomes the right child of x and x becomes parent of z, if it exists
     if(z != NULL){
-        x -> right = z; z -> parent = x;
+        x -> right = z; 
+        z -> parent = x;
     }else{
         x -> right = NULL;
     }
-
+    //fix the parent so that it points to y instead of x
     if(p != NULL){
         if(p -> left == x){
             p -> left = y; 
             y-> parent = p;
-        }
-        if(p -> right == x){
+        }else{
             p -> right = y; 
             y -> parent = p;
         }
     } else {
+        //this means y is the new root; x was the root previously
         y -> parent = NULL;
         root = y; 
     }
-
-    recalc_height(x);
-    recalc_height(y);
-    recalc_size(x);
-    recalc_size(y);
-    recalc_mindata(x);
-    recalc_mindata(y);
+    //fix the height, size, and mindata of the nodes affected
+    Recalc_height(x);
+    Recalc_height(y);
+    Recalc_size(x);
+    Recalc_size(y);
+    Recalc_mindata(x);
+    Recalc_mindata(y);
 
     return y;
 }
@@ -159,22 +152,22 @@ Node *l_rotate(Node *x){
 
 
 //right rotates the input node and returns the new node so we can use it later 
-//this will be used in a zig zig case of rotation [/]
-Node *r_rotate(Node *x){
+Node *R_rotate(Node *x){
 
     Node *p = x -> parent; 
     Node *y = x -> left; 
     Node *z = y -> right; 
-
-    //perform the rotation 
-    y -> right = x; x -> parent = y;
+    //x becomes right child of y; y becomes paren of x 
+    y -> right = x; 
+    x -> parent = y;
+    //z becomes the left child of x and x becomes the parent of z, if it exists
     if(z != NULL){
-        x -> left = z; z -> parent = x;
+        x -> left = z; 
+        z -> parent = x;
     }else{
         x -> left = NULL;
     }
-
-    //fix the top part of the tree
+    //If x had a parent previously, we have to stitch it to y
     if(p != NULL){
         if(p -> left == x){
             p -> left = y; 
@@ -185,26 +178,27 @@ Node *r_rotate(Node *x){
             y -> parent = p;
         }
     } else {
+        //if not, that means x was previously the root, so y is the new root
         y -> parent = NULL;
         root = y;  
     }
-
-    recalc_height(x);
-    recalc_height(y);
-    recalc_size(x);
-    recalc_size(y);
-    recalc_mindata(x);
-    recalc_mindata(y);
+    //fix height, size, and mindata for all nodes affected
+    Recalc_height(x);
+    Recalc_height(y);
+    Recalc_size(x);
+    Recalc_size(y);
+    Recalc_mindata(x);
+    Recalc_mindata(y);
 
     return y;
 
 }
 
+
 //Left rotates a node then right rotates it; will be used in a zig zag case [<] 
 Node *LR_rotate(Node *x){
-    l_rotate(x -> left);
-    Node* node = r_rotate(x);
-
+    L_rotate(x -> left);
+    Node* node = R_rotate(x);
     return node;
 }
 
@@ -212,16 +206,15 @@ Node *LR_rotate(Node *x){
 
 //Right rotates a node then left rotates it; will be used in a zig zag case [>] 
 Node *RL_rotate(Node *x){
-    r_rotate(x -> right);
-    Node* node = l_rotate(x);
-
+    R_rotate(x -> right);
+    Node* node = L_rotate(x);
     return node; 
 }
 
 
 
 //calculates the bf of a node
-int calculateBF(Node *x){
+int CalculateBF(Node *x){
     int rightHeight;
     if(x -> right == NULL){
         rightHeight = 1; 
@@ -237,126 +230,176 @@ int calculateBF(Node *x){
     }
 
     int BF = leftHeight - rightHeight;
-
     return BF;
 }
 
 
 
-//takes a node created by the createNode function and places it in the right spot in the tree. 
-//also takes in the root node; must be created beforehand
-void Insert(Node *x, Node *z){
+//when calling, current is initialized to root
+Node* Insert(Node *current, int insertedKey, int insertedData){
 
-    //if the key to be inserted is greater than root, then we need to insert it in the right
-    if(x -> key < z -> key){
-        if(x -> right){
-            Insert(x -> right, z);
-            recalc_height(x);
-            recalc_height(x);
-            recalc_mindata(x);
-            recalc_size(x);
-        }else{
-            //set the right child to z; set z's parent to x ***
-            x -> right = z; z -> parent = x;
-            heightIncreased = true; 
-            recalc_height(z);
-            recalc_height(x);
-            recalc_mindata(z);
-            recalc_mindata(x);
-            recalc_size(z);
-            recalc_size(x);
-            
-        } 
-        if(heightIncreased){
-
-            //calculate right height 
-            int xBF = calculateBF(x);
-
-            //case 2.1 and case 2.2: no need to do any rotation 
-            if(xBF == 0){
-                heightIncreased = false;
-            }
-        
-            //case 2.3: this is if bf was previously -1 but is now -2
-            else if (xBF == -2){
-
-                int xrBF = calculateBF(x -> right);
-
-                //subcase 1: if the right balance factor is -1
-                if(xrBF == - 1){
-
-                    l_rotate(x); //zig zig case [\]; be sure to update to the new root
-
-                    heightIncreased = false;
-                }
-                //subcase 2: if the right subtree balance factor is 1
-                else if (xrBF == 1) {
-                    int xrlBF = calculateBF(x -> right -> left);
-                    RL_rotate(x);
-                    if(xrBF == -1){
-                        heightIncreased = false; 
-                    }
-                    
-                }
-            }
-        } 
+    //PERFORM NORMAL BST INSERTION RECURSIVELY
+    if(current == NULL){
+        //if we hit a null pointer, return the node
+        return CreateNode(insertedKey, insertedData);
+    }else if(insertedKey < current -> key){
+        //if the node is less than the parent, we need to explore the left subtree
+        current -> left = Insert(current -> left, insertedKey, insertedData);
+    }else{
+        //if the node is greater, we need to explore the right subtree (node: this implementation assumes no duplicates)
+        current -> right = Insert(current -> right, insertedKey, insertedData);
     }
-    //otherwise, if the key is smaller than the root, then we need to insert it in the left
+
+    /*** NOTE: the following lines will complete in a bottom-up manner for all nodes in the insertion path, once recursion stops ***/
+
+    //FIX VALUES FOR EACH NODE IN THE INSERTION PATH
+    Recalc_height(current);
+    Recalc_size(current);
+    Recalc_mindata(current);
+
+    //FIND FIRST UNBALANCED NODE AND FIX THE TREE: 
+    int currentBalanceFactor = CalculateBF(current);
+    //left left case -> "/"
+    if(currentBalanceFactor > 1 && insertedKey > current -> left -> key){
+        return R_rotate(current);
+    }
+    //left right case -> "<"
+    if(currentBalanceFactor > 1 && insertedKey < current -> left -> key){
+        return LR_rotate(current);
+    }
+    //right right case -> "\"
+    if(currentBalanceFactor < -1 && insertedKey < current -> right -> key){
+        return L_rotate(current);
+    }
+    //right left case -> ">"
+    if(currentBalanceFactor < -1 && insertedKey > current -> right -> key){
+        return RL_rotate(current);
+    }
+    
+    return current;
+
+}
+
+
+
+//implement delete
+Node* Delete(Node* currentNode, int key){
+
+    //PERFORM NORMAL BST DELETE
+    if(root == NULL){
+        return currentNode;
+    }
+    if(key < currentNode -> key){
+        return Delete(currentNode -> left, key);
+    } else if(key > currentNode -> key){
+        return Delete(currentNode -> right, key);
+    }
     else{
-        if(x -> left){
-            Insert(x -> left, z);
-            recalc_height(x);
-            recalc_mindata(x);
-            recalc_size(x);
-        
-        }else{
-            //set the right child to z
-            x -> left = z; z -> parent = x;
-            heightIncreased = true; 
-            recalc_height(z);
-            recalc_height(x);
-            recalc_mindata(z);
-            recalc_mindata(x);
-            recalc_size(z);
-            recalc_size(x);
+        //root is node to be deleted
+        Node* parent = currentNode -> parent;
+        //case 1: deleting a leaf node
+        if(!(currentNode -> right) && !(currentNode -> left)){
 
-        } 
-        if(heightIncreased){
-            //calculate the left height
-            int xBF = calculateBF(x);
-
-            //case 2.1 and 2.2: no need to do any rotation
-            if(xBF == 0){
-                bool xBF = 0;
-                heightIncreased = false;
+            //check if the node to be deleted is the root. If not, the node has a parent whose pointers need fixing
+            if(currentNode != root){
+                if(parent->left == currentNode){
+                    parent -> left = NULL;
+                }else{
+                    parent -> right = NULL;
+                }
+            } else{
+                //else the node doesn't have a parent, so just set it to null
+                root = NULL;
             }
+            //finally, free the node's memory
+            free(currentNode);
             
-            //case 2.3: this is if bf was previously 1 but is now 2
-            else if(xBF == 2){
-                bool xBF = 2; 
-                int xlBF = calculateBF(x -> left);
 
-                //subcase 1: if the right balance factor is 1
-                if(xlBF == 1){
-                    r_rotate(x); //zig zig case [/]; be sure to update root to new root
-                    heightIncreased = false;
-                    //no need to fix height for this case
-                }
-                //subcase 2: 
-                else if (xlBF == -1) {
-                    int xlrBF = calculateBF(x -> left -> right);
-                    LR_rotate(x);
-                    if(xlrBF == 1){
-                        heightIncreased = false; 
-                    }
-                    
-                    
-                }
-                    
-                
+        } else if(currentNode -> right && currentNode -> left){
+            //find the sucessor
+            Node* successor = FindSuccessor(currentNode->key, root);
+            //store the value of the successor in a variable
+            int value = successor -> key;
+            //delete the successor (recursively)
+            Delete(root, successor->key);
+            //set the value of successor to the currentNode (recycle the node struct)
+            currentNode -> key = value; 
+        }else{
+            //the current node only has one child; find and store it
+            Node* child; 
+            if(currentNode -> left){
+                child = currentNode -> left;
+            }else{
+                child = currentNode -> right;
             }
-        } 
+
+            //if the node isn't a root, it has a parent whose pointers must be fixed
+            if(currentNode != root){
+                //check if the node is a left child or a right child with respect to its parent
+                if(parent->left == currentNode){
+                    //stitch accordingly
+                    parent -> left = child;
+                }else{
+                    parent -> right = child;
+                }
+            }else{
+                root = child;
+            }
+            free(currentNode);
+        }
+
     }
+
+
+}
+
+
+
+//search is the same as normal BST searching
+Node* Search(int key, Node* root){
+    if(root == NULL || key == root -> key){
+        return root;
+    }else if(key < root -> key){
+        return Search(key, root -> left);
+    }else{
+        return Search(key, root -> right);
+    }
+}
+
+
+
+Node* FindSuccessor(int key, Node* root){
+    Node* node = Search(key, root);
+    Node* successor;
+    //if the node has a right child, just find the minimum of this subtree
+    if(node -> right){
+        successor = FindMinimum(node->right);
+    }else{
+        //otherwise, find the first right ancestor
+        Node* parent = node -> parent; 
+        while(parent && parent -> right == node){
+            node = parent;
+        }
+        successor = node -> parent;
+    }
+    return successor;
+}
+
+
+
+Node* FindMinimum(Node* root){
+    Node* currentNode = root;
+    while(currentNode -> left){
+        currentNode = currentNode -> left;
+    }
+    return currentNode;
+}
+
+
+
+//implement level order printing
+void PrintTreeLevelOrder(){
+
 }
 
 
@@ -377,7 +420,7 @@ Node *LCA(int k1, int k2, Node *x){
 
 
 //finds the minimum in a list of numbers
-int findMin(int *array, int n){
+int FindMin(int *array, int n){
     int min = array[0];
     for(int i = 1; i<n; i++){
         if(array[i] < min){
@@ -389,13 +432,15 @@ int findMin(int *array, int n){
 
 
 
+//finds the minimum element within the range k1 and k2 in the tree
 int RangeMinimum(int k1, int k2, Node *y){
 
+    int maxSize = 100;
     //create a dynamic array for storing the mindata
     int n = 0; //n is the current size of the array
-    int *minList = (int *) malloc(n * sizeof(int));
+    int *minList = (int *) malloc(100 * sizeof(int));
 
-    //find the least common ancsetor and call it x
+    //find the least common ancsetor 
     Node *ancestor = LCA(k1, k2, y);
     n++; 
     minList[n-1] = ancestor -> data;
@@ -447,7 +492,7 @@ int RangeMinimum(int k1, int k2, Node *y){
         }
     }
 
-    return findMin(minList, n);
+    return FindMin(minList, n);
 
 }
 
@@ -471,20 +516,20 @@ int main(int argc, char *argv[]){
     while(file >> instruction >> arg1 >> arg2){
 
         if(instruction == "IN"){
-            counter ++;
+            ++counter;
             //if we're making the first insertion, create the root node
             if(counter == 1){
-                root = createNode(arg1, arg2);
+                root = CreateNode(arg1, arg2);
             }
             //otherwise, insert a node.
             else{
-                Insert(root, createNode(arg1, arg2));
+                Insert(root, arg1, arg2);
             }
-        } else{
+        } else if(instruction == "RM"){
             min = RangeMinimum(arg1, arg2, root);  
+            printf("Found minimum: %d\n", min);
         }
     }
-    printf("%d\n", min);
 
     return 0;
 
